bfbfd7455915d1544519edb2537835cc26b7d704
se0413 <tldms7133@naver.com>
2024-09-26 17:56:21 +0900

    0926.2

--- samcheck/js/common.js
+++ samcheck/js/common.js
@@ -71,5 +71,10 @@ $(document).ready(function(){
         }
     });
     
-
+    $('.header_top .tnb_r button.lang_open').click(function(){
+        $('.header_top .tnb_r').addClass('on')
+    })
+    $('.header_top .tnb_r button.lang_close').click(function(){
+        $('.header_top .tnb_r').removeClass('on')
+    })
 })
