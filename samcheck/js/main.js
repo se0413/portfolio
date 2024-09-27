1b21ef5f49f386983d6bd588055a97e963804678
se0413 <tldms7133@naver.com>
2024-09-27 13:11:09 +0900

    0927.2

--- samcheck/js/main.js
+++ samcheck/js/main.js
@@ -25,33 +25,30 @@ $(document).ready(function(){
 
     });
 
-    const swiper = new Swiper('.swiper', { 
-        slidesPerView: "auto", 
+    const tap_cnt_swiper = new Swiper('.tap_cnt .swiper', { 
         spaceBetween: 16, 
         breakpoints: {
-            640: {  /* 640px 이상이 되면 적용 */
+            640: {  
                 spaceBetween: 30, 
+                slidesPerView: 3,
+
             },
-            1024: {  /* 1024px 이상이 되면 적용 */
-                spaceBetween: 40,
+            1024: {
+                spaceBetween: 24,
+                slidesPerView: 4,
+
+            },
+            1300: {
+                spaceBetween: 24,
+                slidesPerView: 4,
+
             },
         },
-        centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
-        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-        autoplay: {  /* 팝업 자동 실행 */
-            delay: 2500,
-            disableOnInteraction: true,
-        },
+        centeredSlides: false, 
+        loop: true,  
         navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
-            el: '.swiper-pagination', /* 해당 요소의 class명 */
-            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
-            type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+            nextEl: '.btn_next',
+            prevEl: '.btn_prev',
         },
     });
-    swiper.autoplay.stop();  /* 일시정지 기능 */
-    swiper.autoplay.start();  /* 재생 기능 */
 })
