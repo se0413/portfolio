df5121f57572a2e58c9b634346cbfe8b984f3fa5
cat0413 <tldms7133@naver.com>
2024-09-28 18:39:28 +0900

    0928.2

--- portfolio_2/js/main.js
+++ portfolio_2/js/main.js
@@ -1,30 +1,44 @@
 $(document).ready(function(){
 
-		let device_status
-		let window_w
-		function device_chk(){
-			window_w = $(window).width()
-			if(window_w > 1024 ){ //pc
-				device_status = 'pc'
-			}else{ //모바일
-				device_status = 'mobile'
+	let menuName = $('.header')  // 상단에 고정할 메뉴 영역 선택자
+	let menuItem = $('.header ul li') // data-link 값을 준 클릭할 요소의 선택자
+	let sectionName
+	let moveTop
+	let areaTop
+	let areaH
+	let areaName
+	let scrollTop
+	menuItem.on('click', function(){
+		sectionName = $(this).attr('data-link')
+		moveTop = $('*[data-menu="'+sectionName+'"]').offset().top - menuName.height()
+		$('html, body').animate({
+			scrollTop : moveTop
+		}, 500)
+	})
+	menuChk()
+	$(window).scroll(function(){
+		menuChk()
+	})
+	function menuChk(){
+		scrollTop = $(window).scrollTop()
+		$.each($('*[data-menu]'), function(idx, item){
+			areaTop = $('*[data-menu]').eq(idx).offset().top
+			areaH = $('*[data-menu]').eq(idx).height()
+			areaName = $('*[data-menu]').eq(idx).attr('data-menu')
+			if((scrollTop >= areaTop - menuName.height()) && (scrollTop < areaTop + areaH - menuName.height())){
+				menuItem.removeClass('active')
+				menuItem.siblings('[data-link="'+areaName+'"]').addClass('active')
+			}else if(scrollTop < $('*[data-menu]').first().offset().top){
+				menuItem.removeClass('active')
+			}else if(scrollTop > $('*[data-menu]').last().offset().top + $('*[data-menu]').last().height()){
+				menuItem.removeClass('active')
 			}
-		}
-		device_chk()//문서가 로딩되고 1번 실행
-		$(window).resize(function(){ //문서가 리사이즈될 때마다 1번씩 실행
-			device_chk()
-		})
-		
-		$('.header .gnb_wrap ul li[data-link="main"]').on('click', function(){
-			swiper.slideTo(0, 500)
 		});
+	}
 
-		var container_swiper = new Swiper(".container .swiper", {
-			slidesPerView: 1,
-			spaceBetween: 0,
-			mousewheel: true,
-			freeMode: false,
-			
-		});
-		
+	AOS.init({
+        offset: 400, // 해당 콘텐츠가 하단에서 몇 px 위로 올라와에 나타나는 효과가 나타날지 셋팅하는 값
+        duration: 850, // 애니메이션 효과가 작동되는 시간
+        easing: 'linear', // 가속도
+    });
 })
